7ab472e24c3e8daf3abeecd91c7ecf89cc6e6820
jomrembau <jomir.bautista@gmail.com>
2026-03-08 10:31:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SayHello(new string[] { "JR", "Mahinay" }, "Chicago", "Illinois"));
+            Console.ReadLine();
+        }
+
+
+        
+        public static string SayHello(string[] name, string city, string state)
+        {
+            string text = String.Join(" ", name);
+            return $"Hello, {text}! Welcome to {city}, {state}!";
+        }
+        
+
+    }
+}
